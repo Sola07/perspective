71d5272c2ad64bb6aca9db4d763df5651c6d9765
Flavie Guiselin <flavie.sola.guiselin@gmail.com>
2024-03-27 11:31:53 +0100

    beautiful seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,33 @@
 require "faker"
 
-3.times do
-  user = User.new(
-    email: Faker::Internet.email,
-    password: Faker::Internet.password,
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name
-  )
-  user.save
-end
+Simulation.destroy_all
+User.destroy_all
 
-user2 = User.new(
+
+user = User.new(
   email: "toto@gmail.com",
   password: "tototo",
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name
 )
-user2.save
+user.save
 
-3.times do
-  simulation = Simulation.new(
-    prix_du_bien: Faker::Number.number(digits: 6),
-    prix_travaux_cont: Faker::Number.number(digits: 5),
-    prix_travaux_renov: Faker::Number.number(digits: 4),
-    achat_meubles: Faker::Number.number(digits: 4),
-    frais_achat: Faker::Number.number(digits: 4),
-    apport: Faker::Number.number(digits: 5),
-    duree_credit_an: 25,
-    taux_interet: Faker::Number.decimal(l_digits: 2),
-    taux_assurance: Faker::Number.decimal(l_digits: 2),
-    loyer_hc: Faker::Number.number(digits: 4),
-    taxe_fonciere: Faker::Number.number(digits: 4),
-    charges_locatives: Faker::Number.number(digits: 3),
-    autres_charges: Faker::Number.number(digits: 3),
-    revenu_net_global: Faker::Number.number(digits: 5),
-    user_id: (1..3).to_a.sample
+
+simulation = Simulation.new(
+  prix_du_bien: 60000,
+  prix_travaux_cont: 50000,
+  prix_travaux_renov: 0,
+  achat_meubles: 0,
+  frais_achat: 1500,
+  apport: 15000,
+  duree_credit_an: 25,
+  taux_interet: 4.1,
+  taux_assurance: 0.35,
+  loyer_hc: 800,
+  taxe_fonciere: 1200,
+  charges_locatives: 480,
+  autres_charges: 0,
+  revenu_net_global: 70000,
+  user_id: 1
   )
   simulation.save!
-end
